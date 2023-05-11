e3a33d2a479fcecc46ae1e2b78c5a76892ec7fa9
Patrick Bowen <phunanon@users.noreply.github.com>
2023-05-11 08:54:48 +0100

    Initial version

---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import assert from "assert";
+import dotenv from "dotenv";
+import { ChannelType, Client } from "discord.js";
+import express from "express";
+dotenv.config();
+assert(process.env.DISCORD_TOKEN, "DISCORD_TOKEN is required");
+
+const app = express();
+const client = new Client({ intents: ["GuildMessages"] });
+const port = 3000;
+
+(async function () {
+  client.once("ready", () => console.log("Discord ready"));
+  client.login(process.env.DISCORD_TOKEN);
+})();
+
+app.listen(port, () => {
+  console.log(`Sever ready: http://localhost:${port}`);
+});
+
+app.get("/:guildId/:channelId", async (req, res) => {
+  const guildId = req.params.guildId;
+  const channelId = req.params.channelId;
+  if (
+    !Number.isInteger(Number(guildId)) ||
+    !Number.isInteger(Number(channelId))
+  ) {
+    res.status(400).send("guildId and channelId must be integers");
+    return;
+  }
+  const guild = await client.guilds.fetch(guildId);
+  if (!guild) {
+    res.status(400).send("Guild not found");
+    return;
+  }
+  const channel = await guild.channels.fetch(channelId);
+  if (!channel) {
+    res.status(400).send("Channel not found");
+    return;
+  }
+  if (channel.type !== ChannelType.GuildForum) {
+    res.status(400).send("Channel is not a forum");
+    return;
+  }
+  const threads = await channel.threads.fetch();
+  threads.threads.reverse();
+  const threadsWithData = await Promise.all(
+    threads.threads.map(async thread => {
+      const { id, createdTimestamp, name, ownerId } = thread;
+      const owner = ownerId ? await guild.members.fetch(ownerId) : null;
+      const [lastMessage] = (
+        await thread.messages.fetch({ limit: 1 })
+      ).values();
+      return {
+        ...{ id, title: name, createdTimestamp },
+        author: owner?.user.username,
+        lastTimestamp: lastMessage?.createdTimestamp ?? 0,
+      };
+    })
+  );
+  threadsWithData.sort((a, b) => b.lastTimestamp - a.lastTimestamp);
+  res.send(threadsWithData);
+});
+
+app.get("/:guildId/:channelId/:threadId", async (req, res) => {
+  const threadId = req.params.threadId;
+  const guildId = req.params.guildId;
+  const channelId = req.params.channelId;
+  if (
+    !Number.isInteger(Number(guildId)) ||
+    !Number.isInteger(Number(channelId)) ||
+    !Number.isInteger(Number(threadId))
+  ) {
+    res.status(400).send("guildId, channelId, threadId must be integers");
+    return;
+  }
+  const guild = await client.guilds.fetch(guildId);
+  if (!guild) {
+    res.status(400).send("Guild not found");
+    return;
+  }
+  const channel = await guild.channels.fetch(channelId);
+  if (!channel) {
+    res.status(400).send("Channel not found");
+    return;
+  }
+  if (channel.type !== ChannelType.GuildForum) {
+    res.status(400).send("Channel is not a forum");
+    return;
+  }
+  const threads = await channel.threads.fetch();
+  const thread = threads.threads.find(x => x.id === threadId);
+  if (!thread) {
+    res.status(400).send("Thread is unavailable");
+    return;
+  }
+  const messageData = await thread.messages.fetch({ limit: 100 });
+  messageData.reverse();
+  const messages = messageData.map(
+    ({ cleanContent, author, createdTimestamp }) => ({
+      createdTimestamp,
+      content: cleanContent,
+      author: author.username,
+    })
+  );
+  res.send(messages);
+});
