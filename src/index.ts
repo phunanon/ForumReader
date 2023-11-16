cf9174d6766eae9e8cf73b88898f5f7172db4d0b
Patrick Bowen <phunanon@users.noreply.github.com>
2023-11-16 23:01:49 +0000

    Fetch at least ten archived channels

--- src/index.ts
+++ src/index.ts
@@ -24,105 +24,111 @@ app.listen(port, () => {
   console.log(`Sever ready: http://localhost:${port}`);
 });
 
-app.get("/:guildId/:channelId", async (req, res) => {
-  const guildId = req.params.guildId;
-  const channelId = req.params.channelId;
-  if (
-    !Number.isInteger(Number(guildId)) ||
-    !Number.isInteger(Number(channelId))
-  ) {
-    res.status(400).send("guildId and channelId must be integers");
-    return;
-  }
-  const guild = await client.guilds.fetch(guildId);
-  if (!guild) {
-    res.status(400).send("Guild not found");
-    return;
-  }
-  const channel = await guild.channels.fetch(channelId);
-  if (!channel) {
-    res.status(400).send("Channel not found");
-    return;
-  }
-  if (channel.type !== ChannelType.GuildForum) {
-    res.status(400).send("Channel is not a forum");
-    return;
-  }
-  const threads = await channel.threads.fetch();
-  threads.threads.reverse();
-  const threadsWithData = await Promise.all(
-    threads.threads.map(async thread => {
-      const { id, createdTimestamp, name, ownerId, messageCount } = thread;
-      const owner = ownerId ? await fetchMember(guild, ownerId) : null;
-      const [lastMessage] = (
-        await thread.messages.fetch({ limit: 1 })
-      ).values();
-      return {
-        ...{ id, title: name, createdTimestamp },
-        author: owner?.user.username ?? `Unknown (${ownerId})`,
-        lastTimestamp: lastMessage?.createdTimestamp ?? 0,
-        messageCount,
-      };
-    })
-  );
-  threadsWithData.sort((a, b) => b.lastTimestamp - a.lastTimestamp);
-  res.send(threadsWithData);
+app.get("/:guildId/:channelId", (req, res) => {
+  (async () => {
+    const guildId = req.params.guildId;
+    const channelId = req.params.channelId;
+    if (
+      !Number.isInteger(Number(guildId)) ||
+      !Number.isInteger(Number(channelId))
+    ) {
+      res.status(400).send("guildId and channelId must be integers");
+      return;
+    }
+    const guild = await client.guilds.fetch(guildId);
+    if (!guild) {
+      res.status(400).send("Guild not found");
+      return;
+    }
+    const channel = await guild.channels.fetch(channelId);
+    if (!channel) {
+      res.status(400).send("Channel not found");
+      return;
+    }
+    if (channel.type !== ChannelType.GuildForum) {
+      res.status(400).send("Channel is not a forum");
+      return;
+    }
+    const threads = [
+      ...(await channel.threads.fetch()).threads.values(),
+      ...(await channel.threads.fetchArchived({ limit: 10 })).threads.values(),
+    ];
+    threads.reverse();
+    const threadsWithData = await Promise.all(
+      threads.map(async thread => {
+        const { id, createdTimestamp, name, ownerId, messageCount } = thread;
+        const owner = ownerId ? await fetchMember(guild, ownerId) : null;
+        const [lastMessage] = (
+          await thread.messages.fetch({ limit: 1 })
+        ).values();
+        return {
+          ...{ id, title: name, createdTimestamp },
+          author: owner?.user.username ?? `Unknown (${ownerId})`,
+          lastTimestamp: lastMessage?.createdTimestamp ?? 0,
+          messageCount,
+        };
+      })
+    );
+    threadsWithData.sort((a, b) => b.lastTimestamp - a.lastTimestamp);
+    res.send(threadsWithData);
+  })();
 });
 
-app.get("/:guildId/:channelId/:threadId", async (req, res) => {
-  const threadId = req.params.threadId;
-  const guildId = req.params.guildId;
-  const channelId = req.params.channelId;
-  if (
-    !Number.isInteger(Number(guildId)) ||
-    !Number.isInteger(Number(channelId)) ||
-    !Number.isInteger(Number(threadId))
-  ) {
-    res.status(400).send("guildId, channelId, threadId must be integers");
-    return;
-  }
-  const guild = await client.guilds.fetch(guildId);
-  if (!guild) {
-    res.status(400).send("Guild not found");
-    return;
-  }
-  const channel = await guild.channels.fetch(channelId);
-  if (!channel) {
-    res.status(400).send("Channel not found");
-    return;
-  }
-  if (channel.type !== ChannelType.GuildForum) {
-    res.status(400).send("Channel is not a forum");
-    return;
-  }
-  const threads = await channel.threads.fetch();
-  const thread = threads.threads.find(x => x.id === threadId);
-  if (!thread) {
-    res.status(400).send("Thread is unavailable");
-    return;
-  }
+app.get("/:guildId/:channelId/:threadId", (req, res) => {
+  (async () => {
+    const threadId = req.params.threadId;
+    const guildId = req.params.guildId;
+    const channelId = req.params.channelId;
+    if (
+      !Number.isInteger(Number(guildId)) ||
+      !Number.isInteger(Number(channelId)) ||
+      !Number.isInteger(Number(threadId))
+    ) {
+      res.status(400).send("guildId, channelId, threadId must be integers");
+      return;
+    }
+    const guild = await client.guilds.fetch(guildId);
+    if (!guild) {
+      res.status(400).send("Guild not found");
+      return;
+    }
+    const channel = await guild.channels.fetch(channelId);
+    if (!channel) {
+      res.status(400).send("Channel not found");
+      return;
+    }
+    if (channel.type !== ChannelType.GuildForum) {
+      res.status(400).send("Channel is not a forum");
+      return;
+    }
+    const thread = await channel.threads.fetch(threadId);
+    if (!thread) {
+      res.status(400).send("Thread is unavailable");
+      return;
+    }
 
-  let messageData: Collection<string, Message<true>> | undefined;
-  let lastMessageId: string | undefined;
-  do {
-    const messages = await thread.messages.fetch({
-      limit: 100,
-      before: lastMessageId,
-    });
-    messageData = messageData ? messageData.concat(messages) : messages;
-    lastMessageId = messages.last()?.id;
-  } while (lastMessageId);
+    let messageData: Collection<string, Message<true>> | undefined;
+    let lastMessageId: string | undefined;
+    do {
+      const messages = await thread.messages.fetch({
+        limit: 100,
+        before: lastMessageId,
+      });
+      messageData = messageData ? messageData.concat(messages) : messages;
+      lastMessageId = messages.last()?.id;
+    } while (lastMessageId);
 
-  messageData.reverse();
-  const messages = messageData.map(
-    ({ cleanContent, author, createdTimestamp, attachments }) => ({
-      createdTimestamp,
-      content: cleanContent,
-      author: author.username,
-      attachments: attachments.map(a => a.url),
-    })
-  );
-  res.send(messages);
+    messageData.reverse();
+    const messages = messageData.map(
+      ({ cleanContent, author, createdTimestamp, attachments }) => ({
+        createdTimestamp,
+        content: cleanContent,
+        author: author.username,
+        attachments: attachments.map(a => a.url),
+      })
+    );
+    res.send(messages);
+  })();
 });
 
 const fetchMember = async (guild: Guild, username: string) => {
